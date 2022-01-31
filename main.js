225bdb36879d1515169170cae41e5d7ffe41aa93
Nikola Stanin <nikolastanin@gmail.com>
2022-02-01 00:48:57 +0100

    added transitions and carousel containers

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 
 
-const rightBtn = document.getElementById("right-btn");
-const leftBtn = document.getElementById("left-btn");
-console.log(rightBtn, leftBtn);
+// const rightBtn = document.getElementById("right-btn");
+// const leftBtn = document.getElementById("left-btn");
+// console.log(rightBtn, leftBtn);
+// const carouselFunc = () =>{
+//     console.log("radi");
+// }
+// rightBtn.addEventListener("click", carouselFunc);
+// leftBtn.addEventListener("click", carouselFunc);
+
+
+const carousel = document.querySelectorAll(".carousel__container");
+console.log(carousel);
+
+carousel.forEach(element => {
+    console.log(element.childNodes);
+});
