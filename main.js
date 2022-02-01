8712c64f2da99be0ac3636fa0f6d9cceddcad8cd
Nikola Stanin <nikolastanin@gmail.com>
2022-02-02 00:34:06 +0100

    JavaScript for carousel func

--- main.js
+++ main.js
@@ -1,18 +1,59 @@
 
 
-// const rightBtn = document.getElementById("right-btn");
-// const leftBtn = document.getElementById("left-btn");
-// console.log(rightBtn, leftBtn);
-// const carouselFunc = () =>{
-//     console.log("radi");
-// }
-// rightBtn.addEventListener("click", carouselFunc);
-// leftBtn.addEventListener("click", carouselFunc);
+let urls = ["./images/desktop-image-hero-1.jpg",
+"./images/desktop-image-hero-2.jpg",
+"./images/desktop-image-hero-3.jpg",];
+const images = document.querySelector(".hero");
+const text_items = document.querySelectorAll(".hero-text__item");
+console.log(images);
+console.log(text_items);
+const rightBtn = document.querySelector("#right-btn");
+const leftBtn = document.querySelector("#left-btn");
+var i =0;
+//default image and text
+window.addEventListener("load", function () {
+  text_items[0].className = "hero__enabled";
+    // hero.style.backgroundImage = `url("${bgImages[count]}")`;
+  });
 
+//functions for carousel
 
-const carousel = document.querySelectorAll(".carousel__container");
-console.log(carousel);
+const nextItem = () =>{
 
-carousel.forEach(element => {
-    console.log(element.childNodes);
-});
+    if(i == text_items.length-1){
+       
+       i=0;
+    }
+    else{
+       
+        i++;
+    }
+    resetElements();
+    text_items[i].className ="hero__enabled";
+    images.style.backgroundImage = `url("${urls[i]}")`;
+}
+
+const prevItem = () =>{
+    console.log("prev");
+    if(i!==0){
+        i -=1;
+    }
+    else{
+        i = urls.length - 1;
+    }
+    resetElements();
+    text_items[i].className ="hero__enabled";
+    images.style.backgroundImage = `url("${urls[i]}")`;
+}
+
+//event listeners for buttons
+rightBtn.addEventListener("click",nextItem);
+leftBtn.addEventListener("click",prevItem);
+
+  //reverting classes back to disabled
+  const resetElements = () =>{
+  text_items.forEach(element => {
+      element.className ="hero__disabled";
+  });
+
+}
